ac5873ff1d2e703ae77eabc95028045e66faa3f7
TomBor <thomas2ansart@gmail.com>
2021-11-08 09:28:12 +0100

    integrate Zoom component inside Map component

--- src/lib/map/Map.svelte
+++ src/lib/map/Map.svelte
@@ -1,16 +1,19 @@
 <script>
     import { onMount } from 'svelte'
+
     import { proj } from '../../stores.js'
+    import { regbbox } from '../../stores.js'
+
     import { geoPath } from 'd3-geo'
     import { select } from 'd3-selection'
     import { brush } from 'd3-brush'
+    import { zoom, zoomIdentity } from 'd3-zoom'
 
     import Basemap from './Basemap.svelte'
-    import Zoom from './Zoom.svelte'
 
     let width = 900, height = 600
     let projection
-    const unsubscribe = proj.subscribe(value => { projection = value })
+    const unsubscribeProj = proj.subscribe(value => { projection = value })
 
     $: path = geoPath(projection)
 
@@ -23,6 +26,7 @@
     // $: height = Math.ceil(bbox[1][1] - bbox[0][1])
 
 
+    // BRUSH ------------------- //
     let rx, ry, rw, rh
     function brushed( { selection: [[x0, y0], [x1, y1]] } ) {
         // passer les coordonnées du brush courant
@@ -40,7 +44,50 @@
         .handleSize(24)
         .on("brush", brushed)
 
+    // ------------------------- //
+
+    // ZOOM ------------------- //
+    let regBbox
+    const unsubscribeRegbbox = regbbox.subscribe(d => { regBbox = d })
+
+    let isReady = false
+
+    // paramètres du pan and zoom
+    const zoom2 = zoom()
+            .scaleExtent([0.5, 10]) // min, max du zoom
+            .translateExtent([[0, 0], [width, height]]) // bornes extérieures du translate
+            .on("zoom", ({ transform }) => select("g#zoom").attr("transform", transform))
+
+    
+    // ZOOM SUR UNE ZONE SPÉCIFIQUE
+    function zoomRegion(b) {
+        if (b != null) {
+            // Récupère les coordonnées en pixel de la bbox d'un geosjon
+            const [[x0, y0], [x1, y1]] = path.bounds(b)
+
+            // Voir exemple : https://observablehq.com/@d3/zoom-to-bounding-box
+            select("g#zoom").transition().duration(750).call(
+                zoom2.transform,
+                zoomIdentity
+                    .translate(width / 2, height / 2)
+                    .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+                    .translate(-(x0 + x1) / 2, -(y0 + y1) / 2)
+            ) 
+        }
+    }
+
+    $: if (isReady) { zoomRegion(regBbox) }
+
+
+    const zoomIn = () => select("g#zoom").transition().call(zoom2.scaleBy, 1.5)
+    const zoomOut = () => select("g#zoom").transition().call(zoom2.scaleBy, 1/1.5)
+
+    // ------------------------- //
+
     onMount( () => {
+        isReady = true
+
+        // BRUSH -----
         // initier le cadrage avec d3-brush
         let gBrush = select('#gBrush')
         gBrush
@@ -54,8 +101,12 @@
             .attr("stroke", "none")
             .attr("cursor", "auto")         // rétablit valeur par défaut
             .attr("pointer-events", "none") // ne bloque/cache pas les events en dessous (ici le zoom)
+
+        // ZOOM -----
+        // applique le zoom sur le groupe "g#zoom" plutôt que l'ensemble du svg
+        // => limite le zoom à l'intérieur du cadrage (via d3-brush)
+        select("g#zoom").call(zoom2);
     })
-    $: height
 </script>
 
 <figure id="svg-container">
@@ -69,9 +120,9 @@
         </defs>
 
         <g id="gCadrage" style="clip-path: url(#clip-cadrage)">
-            <Zoom svgID="#map-svg" {width} {height} {path} >          
+            <g id="zoom" >        
                 <Basemap {path} {outline} />
-            </Zoom>
+            </g>
         </g>
         <g id="gBrush"></g>
             
@@ -88,8 +139,8 @@
     </svg>
 
     <div id="zoom-button">
-        <button><span class="material-icons">add</span></button>
-        <button><span class="material-icons">remove</span></button>
+        <button on:click={zoomIn}><span class="material-icons">add</span></button>
+        <button on:click={zoomOut}><span class="material-icons">remove</span></button>
     </div>
 </figure>
 
@@ -101,6 +152,10 @@
         flex: 1;
         padding: 1rem;
         height: calc(var(--content-h) - 2rem);
+    }
+    
+    g#zoom {
+        cursor: grab;
     }
 
     #zoom-button {
