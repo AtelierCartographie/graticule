df965d08d9a847fb43c94156058f372f5101c179
TomBor <thomas2ansart@gmail.com>
2021-12-10 16:45:55 +0100

    fix: vite config of build target to 'esnext" to support top-level await

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    target: "esnext" // for Top-level await
+  }
 })
